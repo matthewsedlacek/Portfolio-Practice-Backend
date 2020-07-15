aed81c1cfc554cfd0a76add11b66ee92ff580d94
Matthew Sedlacek <sedlacek1991@gmail.com>
2020-07-14 18:03:58 -0700

    adds jwt auth

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -1,15 +1,23 @@
 class Api::V1::UsersController < ApplicationController
-    def create
-        @user = User.create(user_params)
-        if @user.valid?
-            render json: { user: UserSerializer.new(@user) }, status: :created
-        else
-            render json: { error: 'failed to create user' } , status: :not_acceptable
-        end
+    skip_before_action :authorized, only: [:create]
+ 
+    def profile
+        render json: { user: UserSerializer.new(current_user) }, status: :accepted
     end
 
-    private
-    def user_params
-        params.require(:user).permit(:username, :password)
+    def create
+      @user = User.create(user_params)
+      if @user.valid?
+        @token = encode_token(user_id: @user.id)
+        render json: { user: UserSerializer.new(@user), jwt: @token }, status: :created
+      else
+        render json: { error: 'failed to create user' }, status: :not_acceptable
+      end
     end
-end
+   
+    private
+   
+    def user_params
+      params.require(:user).permit(:username, :password)
+    end
+  end
