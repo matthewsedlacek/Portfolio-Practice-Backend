105070d8b51d940a9de570a76ddf4fdc31a032ef
Matthew Sedlacek <sedlacek1991@gmail.com>
2020-07-30 11:40:27 -0700

    adds validations to portfolio and transaction

--- app/models/transaction.rb
+++ app/models/transaction.rb
@@ -1,6 +1,7 @@
 class Transaction < ApplicationRecord
     belongs_to :portfolio
     belongs_to :stock_price
+    validates_numericality_of :quantity, :only_integer => true, :greater_than_or_equal_to => 1
     has_one :user, through: :portfolio
     has_one :company, through: :stock_price
 end
