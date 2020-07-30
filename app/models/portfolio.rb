f63ff9ffb92d58edff916df29b0c0315b1172c95
Matthew Sedlacek <sedlacek1991@gmail.com>
2020-07-30 12:02:13 -0700

    fixes portfolio validation error

--- app/models/portfolio.rb
+++ app/models/portfolio.rb
@@ -2,5 +2,5 @@ class Portfolio < ApplicationRecord
     belongs_to :user
     has_many :transactions
     has_many :stock_prices, through: :transactions
-    validates :starting_value, :numericality => { greater_than_or_equal_to: 100, less_than: 10001 }
+    validates :starting_value, :numericality => { greater_than_or_equal_to: 100 }
 end
