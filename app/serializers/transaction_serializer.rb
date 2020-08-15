8753a735243e50dc1c351d9823d0e87a6f714de8
Matthew Sedlacek <sedlacek1991@gmail.com>
2020-08-15 11:49:20 -0700

    adds created_at to transaction seralizer

--- app/serializers/transaction_serializer.rb
+++ app/serializers/transaction_serializer.rb
@@ -1,5 +1,5 @@
 class TransactionSerializer < ActiveModel::Serializer
-  attributes :id, :portfolio_id, :buy_sell, :quantity, :value, :share_price, :company_id
+  attributes :id, :portfolio_id, :buy_sell, :quantity, :value, :share_price, :company_id, :created_at
   belongs_to :company
 end
 
